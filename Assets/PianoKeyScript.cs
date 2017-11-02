a787d5a9679c1cb4146a1b4b89b91a3a23ad9d79
Tsehsuan Wang <holywrong@gmail.com>
2017-11-02 21:19:17 +0800

    Init

---
+++ Assets/PianoKeyScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PianoKeyScript : MonoBehaviour {
+	public float semitone_offset = 0;
+	public GameObject obj;
+	AudioSource audioSource;
+
+	bool isClicked = false;
+	private GUIStyle guiStyle = new GUIStyle();
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+		obj = gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown () {
+		PlayNote ();
+		RotateKey (90);
+		isClicked = true;
+	}
+
+	void OnMouseUp () {
+		RotateKey (-90);
+		isClicked = false;
+	}
+
+
+	void OnTriggerEnter (Collider c) {
+		PlayNote ();
+		obj.GetComponent<Renderer> ().material.color = Color.red;
+		isClicked = true;
+		//RotateKey (90);
+	}
+
+	void OnTriggerExit (Collider c) {
+		obj.GetComponent<Renderer> ().material.color = Color.white;
+		isClicked = false;
+		//RotateKey (-90);
+	}
+
+	void OnGUI () {
+		guiStyle.fontSize = 25;
+		guiStyle.normal.textColor = Color.red;
+		if (isClicked == true) {
+			GUI.Label (new Rect (Screen.width/2, Screen.height/2+Screen.height/16, 200, 20), this.name, guiStyle);
+		}
+	}
+		
+	void PlayNote () {
+		audioSource.pitch = Mathf.Pow(2f, semitone_offset/12.0f);
+		audioSource.Play();
+	}
+
+	void RotateKey (float degree) {
+		transform.Rotate (new Vector3 (degree, 0, 0), Space.Self);
+	}
+}
---
+++ Assets/PostProcessDepthGrayscale.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//so that we can see changes we make without having to run the game  
+
+[ExecuteInEditMode]
+
+public class PostProcessDepthGrayscale : MonoBehaviour {
+	public Material mat;  
+
+	// Use this for initialization
+	void Start () {
+		Camera.main.depthTextureMode = DepthTextureMode.Depth;
+	}
+
+	void OnRenderImage (RenderTexture source, RenderTexture destination){  
+		Graphics.Blit(source,destination,mat);  
+	}  
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
